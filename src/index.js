251935f6853d439c4942b41d8681bee183c8955c
@s_zhsn <s.zhosan@gmail.com>
2022-04-18 19:32:55 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/main.scss';
+import { Combination } from '../node_modules/js-combinatorics/combinatorics';
+
+const chooseOptimalDistance = (t, k, ls) => {
+  let combinations = new Combination(ls, k);
+  if (combinations.length !== 0) {
+    const totals = [...combinations].map(combination =>
+      combination.reduce((distance, acc) => {
+        return distance + acc;
+      }, 0),
+    );
+    return Math.max(...totals.sort((a, b) => a - b).filter(totalDistance => totalDistance <= t));
+  }
+  return null;
+};
+
+console.log(chooseOptimalDistance(174, 3, [51, 56, 58, 59, 61])); //173
+console.log(chooseOptimalDistance(163, 3, [50])); // null
